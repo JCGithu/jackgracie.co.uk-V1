32e404d5a857de5ba7659f4e0b33b492ef9fafcc
jcgithu <jack.c.gracie@gmail.com>
2021-01-13 20:20:06 +0000

    added idpass

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./**/*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      display: ['Poppins', 'system-ui', 'sans-serif'],
+      body: ['Poppins', 'system-ui', 'sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
