8a6ef60c9432cc8afd399ee73bb874cc944d18e6
jcgithu <jack.c.gracie@gmail.com>
2021-01-14 11:13:13 +0000

    sinonweb

---
+++ assets/js/sinon.js
@@ -0,0 +1,55 @@
+let fail = false;
+var inputText = document.getElementById('input').value;
+let finalText = document.getElementById('output');
+let finalBox = document.getElementById('outputBox');
+let copyButton = document.getElementById('copyButton');
+
+function failUndo() {
+  finalBox.classList.remove('bg-red-400');
+  finalBox.classList.add('bg-green-400');
+  copyButton.innerHTML = 'Copy & Close';
+}
+
+function fail() {
+  console.log('invalid URL');
+  finalBox.classList.remove('bg-green-400');
+  finalBox.classList.add('bg-red-400');
+  finalText.innerHTML = 'Invalid code';
+  copyButton.innerHTML = 'Cancel';
+  fail = true;
+}
+
+function grabURL() {
+  var inputText = document.getElementById('input').value;
+  if (inputText.includes('pscp.tv')) {
+    console.log('valid URL');
+    if (fail) {
+      failUndo();
+    }
+    var regex = /(?<=\/w\/)[0-9A-Za-z]+/g;
+    var token = inputText.match(regex);
+    let URL = `https://api.periscope.tv/api/v2/getAccessPublic?token=${token}`;
+    fetch(URL)
+      .then((data) => {
+        data.json();
+      })
+      .then((json) => {
+        console.log(json);
+        console.log(json.hls_url);
+      });
+    //finalText.innerHTML = copyText;
+  } else {
+    fail();
+  }
+  //console.log(finalText.innerHTML);
+}
+function copy() {
+  if (!fail) {
+    const el = document.createElement('textarea');
+    el.value = finalText.innerHTML;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
+}
