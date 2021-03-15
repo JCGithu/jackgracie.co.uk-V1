ed1f3b2e9575b111b2ba21fcaf83f0c95428feef
jcgithu <jack.c.gracie@gmail.com>
2021-03-15 16:55:29 +0000

    css configs

---
+++ configs/twProd.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./**/*.html', './**/pages/*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      display: ['Poppins', 'system-ui', 'sans-serif'],
+      body: ['Poppins', 'system-ui', 'sans-serif'],
+      sunflower: ['sunflower', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/aspect-ratio')],
+};
