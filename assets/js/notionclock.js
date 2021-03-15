bbbef127aa6bedf25d646494b4fd4dc5749cbf26
jcgithu <jack.c.gracie@gmail.com>
2021-03-15 19:31:28 +0000

    added numbers + suffix

---
+++ assets/js/notionclock.js
@@ -0,0 +1,87 @@
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+function ordinal_suffix_of(i) {
+  var j = i % 10,
+    k = i % 100;
+  if (j == 1 && k != 11) {
+    return i + 'st';
+  }
+  if (j == 2 && k != 12) {
+    return i + 'nd';
+  }
+  if (j == 3 && k != 13) {
+    return i + 'rd';
+  }
+  return i + 'th';
+}
+
+window.onload = displayClock();
+var urlParams;
+(window.onpopstate = function () {
+  var match,
+    pl = /\+/g, // Regex for replacing addition symbol with a space
+    search = /([^&=]+)=?([^&]*)/g,
+    decode = function (s) {
+      return decodeURIComponent(s.replace(pl, ' '));
+    },
+    query = window.location.search.substring(1);
+
+  urlParams = {};
+  while ((match = search.exec(query))) urlParams[decode(match[1])] = decode(match[2]);
+})();
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+function uptimeNowClock() {
+  //console.log(urlParams);
+  let timeNow = new Date();
+  var time = formatAMPM(timeNow);
+  let day = timeNow.getDay();
+  let mDay = ordinal_suffix_of(timeNow.getDate());
+  let month = timeNow.getMonth();
+  var year = timeNow.getFullYear();
+  let source = document.getElementById('time');
+  source.innerHTML = `<span class='text-2xl font-bold tracking-wide'>${time}</span><br><span class='text-sm font-regular'>${days[day]}, ${mDay} ${months[month]}, ${year} </span>`;
+  setTimeout(uptimeNowClock, 1000);
+}
+function displayClock() {
+  let timeDiv = document.createElement('div');
+  timeDiv.id = 'time';
+  document.body.appendChild(timeDiv);
+  uptimeNowClock();
+}
+setTimeout(function () {
+  if (urlParams.bg) {
+    document.body.style.backgroundColor = urlParams.bg;
+  }
+  if (urlParams.bghex) {
+    document.body.style.backgroundColor = `#${urlParams.bghex}`;
+  }
+  if (urlParams.text) {
+    document.body.style.color = urlParams.text;
+  }
+  if (urlParams.texthex) {
+    document.body.style.color = `#${urlParams.texthex}`;
+  }
+}, 1000);
